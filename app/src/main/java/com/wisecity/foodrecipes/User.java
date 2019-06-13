d7adc163c14938e39ae17c9741fa1f13fe7db78d
Berk Utku Yenisey <berkutkuyenisey@gmail.com>
2019-06-13 22:07:58 +0300

    Model class for the user entity has been created.

---
+++ app/src/main/java/com/wisecity/foodrecipes/User.java
@@ -0,0 +1,25 @@
+package com.wisecity.foodrecipes;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("username")
+    private String userName;
+    @SerializedName("password")
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
