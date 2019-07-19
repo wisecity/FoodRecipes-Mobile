e5c7148acb80590aeafd28362261989a773ae675
BerkUtkuYenisey <berkutkuyenisey@gmail.com>
2019-07-20 01:01:01 +0300

    EditRecipe is now working properly.

--- app/src/main/java/com/wisecity/foodrecipes/RestAPI.java
+++ app/src/main/java/com/wisecity/foodrecipes/RestAPI.java
@@ -12,6 +12,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Url;
 
@@ -40,7 +41,7 @@ public interface RestAPI {
     Call<JsonObject> addRecipe( @Header("Authorization") String accessToken, @Body JsonObject body);
 
     @Headers({ "Content-Type: application/json"})
-    @POST("/api/recipeManipulation/{recipe_id}")
+    @PUT("/api/recipeManipulation/{recipe_id}")
     Call<JsonObject> editRecipe( @Header("Authorization") String accessToken, @Path("recipe_id") String recipeId, @Body JsonObject body);
 
     @Headers({ "Content-Type: application/json"})
